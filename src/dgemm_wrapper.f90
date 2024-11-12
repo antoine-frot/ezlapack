36350caa78fa5be2eaaccb7b5078038d80685134
afrot <antoine.frot@orange.fr>
2024-11-12 16:00:27 +0100

    remove module

--- src/dgemm_wrapper.f90
+++ src/dgemm_wrapper.f90
@@ -1,52 +1,44 @@
-program dgemm_wrapper
+subroutine matrices_mult_d(alpha, transa, A, transb, B, beta, C)
+  
+  implicit none
 
-    implicit none
+  ! Inputs
+  character(len=1), intent(in)    :: transa ! 'N' or 'n' for A, else A**T
+  character(len=1), intent(in)    :: transb ! 'N' or 'n' for B, else B**T
 
-    contains
+  double precision, intent(in)    :: alpha
+  double precision, intent(in)    :: beta
 
-    subroutine matrices_mult_d(alpha, transa, A, transb, B, beta, C)
-        
-        implicit none
+  double precision, intent(in)    :: A(:,:)
+  double precision, intent(in)    :: B(:,:)
+  double precision, intent(inout) :: C(:,:)
 
-        ! Inputs
-        character(len=1), intent(in)    :: transa ! 'N' or 'n' for A, else A**T
-        character(len=1), intent(in)    :: transb ! 'N' or 'n' for B, else B**T
+  ! Local variables
+  integer :: m, n, k
+  integer :: lda, ldb, ldc
 
-        double precision, intent(in)    :: alpha
-        double precision, intent(in)    :: beta
+  ! Determine dimensions
+  m = size(C, 1)
+  n = size(C, 2)
+  
+  if (transa == 'N' .or. transa == 'n') then
+    k = size(A, 2)
+    lda = m
+  else
+    k = size(A, 1)
+    lda = k
+  end if
 
-        double precision, intent(in)    :: A(:,:)
-        double precision, intent(in)    :: B(:,:)
-        double precision, intent(inout) :: C(:,:)
+  if (transb == 'N' .or. transb == 'n') then
+    ldb = k
+  else
+    ldb = n
+  end if
 
-        ! Local variables
-        integer :: m, n, k
-        integer :: lda, ldb, ldc
+  ldc = m
 
-        ! Determine dimensions
-        m = size(C, 1)
-        n = size(C, 2)
-        
-        if (transa == 'N' .or. transa == 'n') then
-            k = size(A, 2)
-            lda = m
-        else
-            k = size(A, 1)
-            lda = k
-        end if
+  ! Call the DGEMM routine
+  call dgemm(transa, transb, m, n, k, alpha, A, lda, B, ldb, beta, C, ldc)
 
-        if (transb == 'N' .or. transb == 'n') then
-            ldb = k
-        else
-            ldb = n
-        end if
-
-        ldc = m
-
-        ! Call the DGEMM routine
-        call dgemm(transa, transb, m, n, k, alpha, A, lda, B, ldb, beta, C, ldc)
-
-    end subroutine matrices_mult_d
-
-end program dgemm_wrapper
+end subroutine matrices_mult_d
 
