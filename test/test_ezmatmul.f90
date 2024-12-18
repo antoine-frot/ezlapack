4a72f941fb1ebcf74b9881297c5ea6be4bbdf1c0
antoine-frot <antoine.frot@orange.fr>
2024-12-18 02:43:32 +0100

    Add test_passed variable to see if all tests pass

--- test/test_ezmatmul.f90
+++ test/test_ezmatmul.f90
@@ -1,17 +1,20 @@
 program test_ezmatmul
   !
   ! Test file for ezmatmul
-  use module_ezmatmul
-  use m
+  use module_ezmatmul,       only ezmatmul
+  use module_random_complex, only random_complex
 
   implicit none
 
-  integer          :: N
+  logical                                       :: test_passed
+  integer                                       :: N
     ! Size of the matrices
-  double precision :: alpha, beta
+  double precision                              :: alpha, beta
   double precision, allocatable, dimension(:,:) :: A, B, C, D
+  
 
-  !Test of all default arguments
+  ! Test of all default arguments
+  test_passed = .true.
   N=1000
   print *, "Matrices de taille ", N, " * ", N
   allocate(A(N, N), B(N, N), C(N, N), D(N,N))
@@ -19,65 +22,75 @@ program test_ezmatmul
   call random_number(A)
   call random_number(B)
 
-  !D = matmul(A, B)
   call dgemm('N', 'N', N, N, N, 1d0, A, N, B, N, 0d0, D, N)
 
   call ezmatmul(A, B, C)
   if (any(D /= C)) then
     print *, "Test failed (1)"
-    print *, maxval(abs(D-C))
+    print *, 'Maximal difference between dgemm and ezmatmum: ', maxval(abs(D-C))
+    test_passed = .false.
   end if
 
   call ezmatmul('N', 'N', A, B, C)
   if (any(D /= C)) then
     print *, "Test failed (2)"
-    print *, maxval(abs(D-C))
+    print *, 'Maximal difference between dgemm and ezmatmum: ', maxval(abs(D-C))
+    test_passed = .false.
   end if
 
   call ezmatmul(1d0, A, B, C)
   if (any(D /= C)) then
     print *, "Test failed (3)"
-    print *, maxval(abs(D-C))
+    print *, 'Maximal difference between dgemm and ezmatmum: ', maxval(abs(D-C))
+    test_passed = .false.
   end if
 
   call ezmatmul(A, B, 0d0, C)
   if (any(D /= C)) then
     print *, "Test failed (4)"
-    print *, maxval(abs(D-C))
+    print *, 'Maximal difference between dgemm and ezmatmum: ', maxval(abs(D-C))
+    test_passed = .false.
   end if
 
   call ezmatmul('N', 'N', A, B, 0d0, C)
   if (any(D /= C)) then
     print *, "Test failed (5)"
-    print *, maxval(abs(D-C))
+    print *, 'Maximal difference between dgemm and ezmatmum: ', maxval(abs(D-C))
+    test_passed = .false.
   end if
 
   call ezmatmul('N', 'N', 1d0, A, B, C)
   if (any(D /= C)) then
     print *, "Test failed (6)"
-    print *, maxval(abs(D-C))
+    print *, 'Maximal difference between dgemm and ezmatmum: ', maxval(abs(D-C))
+    test_passed = .false.
   end if
 
   call ezmatmul(1d0, A, B, 0d0, C)
   if (any(D /= C)) then
     print *, "Test failed (7)"
-    print *, maxval(abs(D-C))
+    print *, 'Maximal difference between dgemm and ezmatmum: ', maxval(abs(D-C))
+    test_passed = .false.
   end if
   
-  !Test dgemm directly with ezmatmul
-
-  call random_number(A)
+  ! An additionnal random test with all dummy arguments
+  call random_number(C)
   D = C
-
   call dgemm('T', 'T', N, N, N, 4.7d0, A, N, B, N, 6.9d0, D, N)
   call ezmatmul('T', 'T', 4.7d0, A, B, 6.9d0, C)
   
 
   if (any(D /= C)) then
-    print *, "Test failed: dgemm doesn't match ezmatmul"
-    print *, maxval(abs(D-C))
+    print *, "Random test with all dummy arguments failed for dgemm"
+    print *, 'Maximal difference between dgemm and ezmatmum: ', maxval(abs(D-C))
+    test_passed = .false.
   end if
 
-  print *, "Test finished"
+  print *, "Test finished for dgemm"
+
+  if (.not. test_passed) then
+    print *, '!-------------!'
+    print *, '! TEST FAILED !'
+    print *, '!-------------!'
 
 end program test_ezmatmul
