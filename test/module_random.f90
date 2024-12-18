7d0880b6f76631007acc5b02355d908aebaf7d3a
antoine-frot <antoine.frot@orange.fr>
2024-12-18 15:46:27 +0100

    Ended the module_random

--- test/module_random.f90
+++ test/module_random.f90
@@ -4,55 +4,58 @@ module random
   ! a complex number of single or double precision.!
   !------------------------------------------------!
 
+  implicit none
+
+  private
+  public :: random_complex, random_integer, random_character
+  
   interface random_complex
-    module procedure random_scomplex, randome_dcomplex
+    module procedure random_scomplex, random_dcomplex
   end interface random_complex
 
   subroutine random_scomplex(z)
-    ! Generate a random single precision complex
-    implicit none
-
+    ! Generate a random single precision complex within the unit circle
     complex, intent(inout) :: z
-    real                   :: re, im
 
-    call random_number(re) 
-    call random_number(im)
+    real                   :: r, theta
 
-    z = cmplx(re, im)
+    call random_number(r)
+    r = sqrt(r)
+
+    call random_number(theta)
+    theta = 2.0 * 3.14159265358979323846 * theta
+
+    z = cmplx(r * cos(theta), r * sin(theta))
   end subroutine random_scomplex
 
   subroutine random_dcomplex(z)
-    ! Generate a random double precision complex
-    implicit none
-
+    ! Generate a random double precision complex within the unit circle
     complex*16, intent(inout) :: z
-    double precision          :: re, im
 
-    call random_number(re) 
-    call random_number(im)
+    real(8)                   :: r, theta
 
-    z = cmplx(re, im)
+    call random_number(r)
+    r = sqrt(r)
+
+    call random_number(theta)
+    theta = 2.0 * 3.14159265358979323846 * theta
+
+    z = cmplx(r * cos(theta), r * sin(theta))
   end subroutine random_dcomplex
 
   subroutine random_character(character_array, output)
     ! Get a random character of an array of characters
-    implicit none
-
-    character(len=:), intent(in)    :: character_array
+    character(len=*), intent(in)    :: character_array(:)
     character(len=*), intent(inout) :: output
-    integer                         :: lengh
-    integer                         :: random_int
-    real                            :: random
 
-    lengh = size(character_array)
-    call random_integer(random_int)
-    output = character_array
+    integer                         :: random_int
+
+    call random_integer(1, size(character_array), random_int)
+    output = character_array(random_int)
   end subroutine random_character
 
   subroutine random_integer(lower_bound, upper_bound, output)
     ! Get a random integer between the lowerBound and the upperBound included
-    implicit none
-
     integer, intent(in)    :: lower_bound, upper_bound
     integer, intent(inout) :: output
     
