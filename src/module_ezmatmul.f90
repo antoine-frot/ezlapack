cd2420e80028d1378a11e861361e4a1f03d0f361
antoine-frot <antoine.frot@orange.fr>
2024-12-18 01:32:19 +0100

    Add a beautiful module level docstring

--- src/module_ezmatmul.f90
+++ src/module_ezmatmul.f90
@@ -1,19 +1,52 @@
 module module_ezmatmul
-!-
-! Generic interface for lapack matrice multiplication (*gemm)
-!
-! Type can be real(4), real(8), complex or complex*16
-!
-! subroutine ezmatmul	(	character, optional  :: transa, (default = 'N')
-!                       character, optional  :: transb, (default = 'N')
-!                       type, optional 	     :: alpha,  (default = 1)
-!                       type, dimension(:,:) :: A,
-!                       type, dimension(:,:) :: B,
-!                       type, optional 	     :: beta,   (default = O)
-!                       type, dimension(:,:) :: C,
-!)
-!
-!
+!--------------------------------------------------------------------------------!
+! Purpose:                                                                       ! 
+! Generic interface for lapack matrice multiplication (*gemm)                    ! 
+! i.e. performs C := alpha*op( A )*op( B ) + beta*C                              !     
+! where alpha and beta are scalars, A, B and C are matrices, and                 ! 
+! op( X ) is one of op( X ) = X or op( X ) = X**T or op( X ) = X**H.             !   
+!--------------------------------------------------------------------------------!       
+! Subroutine:                                                                    !         
+! Type can be real(4), real(8), complex or complex*16,                           !           
+! but all dummy arguments should have the same type.                             !   
+!                                                                                !   
+! subroutine ezmatmul	(	character*1, optional (default = 'N') :: transa,         !     
+!                       character*1, optional (default = 'N') :: transb,         !     
+!                       type,        optional (default =  1 ) :: alpha,          !       
+!                       type, dimension(:,:), contiguous      :: A,              !       
+!                       type, dimension(:,:), contiguous      :: B,              !     
+!                       type,        optional (default =  0 ) :: beta,           !         
+!                       type, dimension(:,:), contiguous      :: C,              ! 
+! )                                                                              !  
+!                                                                                ! 
+! transa (character*1, optional (default = 'N')): specifies the form of op( A ). !
+!   if transa = 'N' or 'n',  op( A ) = A.                                        !
+!   if transa = 'T' or 't',  op( A ) = A**T.                                     !
+!   if transa = 'C' or 'c',  op( A ) = A**H.                                     !
+!                                                                                !
+! transb (character*1, optional (default = 'N')): specifies the form of op( B ). !
+!   if transb = 'N' or 'n',  op( B ) = B.                                        !
+!   if transb = 'T' or 't',  op( B ) = B**T.                                     !
+!   if transb = 'C' or 'c',  op( B ) = B**H.                                     !
+!                                                                                !
+! alpha (type, optional (default = 1)): specifies the scalar alpha.              !
+!                                                                                !
+! A (type, dimension(:), contiguous): specifies the matrix A                     !
+!                                                                                !
+! B (type, dimension(:), contiguous): specifies the matrix B                     !
+!                                                                                !
+! beta (type, optional (default = 0)): specifies the scalar beta.                !
+!                                                                                !
+! C (type, dimension(:), contiguous): specifies the matrix C                     !
+!--------------------------------------------------------------------------------!
+! Examples:                                                                      !
+!                                                                                !
+! call ezmatmul(A,B,C) computes C:= A*B with the simplicity of matmul            !
+! while harnessing the high performance of the LAPACK library.                   !
+!                                                                                !
+! On the other hand, ezmatmul keep the versatility of gemm with for example:     !   
+! call ezmatmul('C','T',(5d-4,8d6),A,B,(9d2,3d-7),C)                             !     
+!--------------------------------------------------------------------------------!   
   use, intrinsic :: iso_fortran_env, only: sp=>real32, dp=>real64
 
   implicit none
