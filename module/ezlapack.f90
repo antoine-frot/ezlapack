e5fe1cd63c132ed3e6b6ed5e98b5b99e26c3510c
antoine-frot <antoine.frot@orange.fr>
2024-12-27 23:44:40 +0100

    Add documentation

--- module/ezlapack.f90
+++ module/ezlapack.f90
@@ -1,5 +1,7 @@
 module ezlapack
-    use module_ezmatmul
-    use module_random
+  ! Global module that specify the other modules used in ezlapack
+  ! Allow the user to just import ezlapack and not each module alone for the sake of simplicity
+  use module_ezmatmul
+  use module_random
 end module ezlapack
 
