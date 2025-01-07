80ad12b542dbfb81b246b471f2d8d6466b9ee049
antoine-frot <antoine.frot@orange.fr>
2025-01-07 12:42:51 +0100

    Change documentation do doxygen documentation

--- module/module_random.f90
+++ module/module_random.f90
@@ -1,44 +1,44 @@
+!> \file
+!> \brief Provides a generic interface for generating random complex numbers, integers, and characters.
+!> 
+!> \details
+!> This module offers utilities for generating random values with specific properties:
+!> - Random complex numbers within the unit circle.
+!> - Random integers within a user-specified range.
+!> - Random selection from an array of characters.
 module module_random
-  !-------------------------------------------------------------------------
-  ! Purpose: Provides a generic interface for generating random complex 
-  !          numbers, integers, and characters.
-  !
-  ! Description:
-  ! This module offers utilities for generating random values with specific
-  ! properties:
-  ! - Random complex numbers within the unit circle.
-  ! - Random integers within a user-specified range.
-  ! - Random selection from an array of characters.
-  !
-  ! Public Subroutines:
-  ! - random_complex(output_complex)
-  !   Generate random complex numbers (single or double precision) within
-  !   the unit circle. Supports scalars and arrays up to rank-2.
-  !
-  ! - random_integer(low, high, output_integer)
-  !   Generate a random integer between 'low' and 'high', inclusive.
-  !
-  ! - random_character(character_array, output_character)
-  !   Randomly select and return a character from an input array of strings.
-  !-------------------------------------------------------------------------
 
   implicit none
 
   private
   public :: random_complex, random_integer, random_character
-  
-  ! Generic interface for random_complex
-  ! When random_complex is called, the program checks which of the following subroutines has the correct arguments.
+
+  !> \brief Generic interface for generating random complex numbers.
+  !>
+  !> \details
+  !> The `random_complex` interface generates random
+  !> complex numbers (single or double precision) within the unit circle.
+  !> Supports scalars and arrays up to rank-2.
+  !>
+  !> ```fortran
+  !> call random_complex(z)
+  !> ```
+  !>
+  !> \param[out] z The random complex number(s), which can be:
+  !> - A single complex number.
+  !> - A rank-1 array of complex numbers.
+  !> - A rank-2 array of complex numbers.
   interface random_complex
     module procedure random_scomplex, random_dcomplex
     module procedure random_scomplex_rank1, random_dcomplex_rank1
     module procedure random_scomplex_rank2, random_dcomplex_rank2
   end interface random_complex
 
-  contains 
+  contains
 
+  !> \brief Generate a random single precision complex number within the unit circle.
+  !> \param[out] z Single precision complex number output.
   subroutine random_scomplex(z)
-    ! Generate a random single precision complex within the unit circle.
     complex(4), intent(out) :: z
 
     real(4)                 :: r, theta
@@ -52,9 +52,9 @@ module module_random
     z = cmplx(r * cos(theta), r * sin(theta), kind=4)
   end subroutine random_scomplex
 
+  !> \brief Generate random single-precision complex numbers for a rank-1 array.
+  !> \param[out] z Rank-1 array of single precision complex numbers.
   subroutine random_scomplex_rank1(z)
-    ! Generate random single-precision complex numbers within the unit circle
-    ! for a rank-1 array.
     complex(4), intent(out) :: z(:)
 
     integer                 :: i
@@ -65,9 +65,9 @@ module module_random
 
   end subroutine random_scomplex_rank1
 
+  !> \brief Generate random single-precision complex numbers for a rank-2 array.
+  !> \param[out] z Rank-2 array of single precision complex numbers.
   subroutine random_scomplex_rank2(z)
-    ! Generate random single-precision complex numbers within the unit circle
-    ! for a rank-2 array.
     complex(4), intent(out) :: z(:,:)
 
     integer                 :: i, j
@@ -80,8 +80,9 @@ module module_random
 
   end subroutine random_scomplex_rank2
 
+  !> \brief Generate a random double precision complex number within the unit circle.
+  !> \param[out] z Double precision complex number output.
   subroutine random_dcomplex(z)
-    ! Generate a random double precision complex within the unit circle.
     complex(8), intent(out) :: z
 
     real(8)                 :: r, theta
@@ -95,9 +96,9 @@ module module_random
     z = cmplx(r * cos(theta), r * sin(theta), kind=8)
   end subroutine random_dcomplex
 
+  !> \brief Generate random double-precision complex numbers for a rank-1 array.
+  !> \param[out] z Rank-1 array of double precision complex numbers.
   subroutine random_dcomplex_rank1(z)
-    ! Generate random double-precision complex numbers within the unit circle
-    ! for a rank-1 array.
     complex(8), intent(out) :: z(:)
 
     integer                 :: i
@@ -108,9 +109,9 @@ module module_random
 
   end subroutine random_dcomplex_rank1
 
+  !> \brief Generate random double-precision complex numbers for a rank-2 array.
+  !> \param[out] z Rank-2 array of double precision complex numbers.
   subroutine random_dcomplex_rank2(z)
-    ! Generate random double-precision complex numbers within the unit circle
-    ! for a rank-2 array.
     complex(kind=8), intent(out) :: z(:,:)
 
     integer                      :: i, j
@@ -123,26 +124,32 @@ module module_random
 
   end subroutine random_dcomplex_rank2
 
-  subroutine random_character(character_array, output)
-    ! Randomly select and return a character from an input array of strings.
+  !> \brief Randomly select and return a character from an input array of strings.
+  !> \param[in] character_array Input array of strings to select from.
+  !> \param[out] output_character Randomly selected character from the input array.
+  subroutine random_character(character_array, output_character)
     character(len=*), intent(in)  :: character_array(:)
-    character(len=*), intent(out) :: output
+    character(len=*), intent(out) :: output_character
 
     integer                       :: random_int
 
     call random_integer(1, size(character_array), random_int)
-    output = character_array(random_int)
+    output_character = character_array(random_int)
   end subroutine random_character
 
-  subroutine random_integer(lower_bound, upper_bound, output)
-    ! Get a random integer between the lowerBound and the upperBound included.
+  !> \brief Get a random integer between the lower and upper bounds (inclusive).
+  !> \param[in] lower_bound Lower bound of the random integer range.
+  !> \param[in] upper_bound Upper bound of the random integer range.
+  !> \param[out] output_integer Random integer within the specified range.
+  subroutine random_integer(lower_bound, upper_bound, output_integer)
     integer, intent(in)    :: lower_bound, upper_bound
-    integer, intent(out)   :: output
-    
+    integer, intent(out)   :: output_integer
+
     real                   :: rand
 
     call random_number(rand)
-    output = lower_bound + int(rand * (upper_bound - lower_bound + 1))
+    output_integer = lower_bound + int(rand * (upper_bound - lower_bound + 1))
   end subroutine random_integer
 
 end module module_random
+
