f5642e107ca92703802d29a37b16dafa8babb297
antoine-frot <antoine.frot@orange.fr>
2025-01-08 12:54:55 +0100

    Change module level docstring to doxygen module level docstring

--- module/ezlapack.f90
+++ module/ezlapack.f90
@@ -1,6 +1,17 @@
+!> \brief Global module that specifies the other modules used in ezlapack
+!> 
+!> This module allows the user to import the `ezlapack` module as a whole, 
+!> simplifying the process by not requiring individual imports of other modules.
+!> It consolidates the necessary modules used within the `ezlapack` library.
+!> 
+!> \details
+!> The `ezlapack` module includes:
+!> - `module_ezmatmul`: A module for matrix multiplication operations.
+!> - `module_random`: A module for random number generation and related utilities.
+!> 
+!> By using `ezlapack`, users can access all the functionality provided by 
+!> the underlying modules without needing to import them separately.
 module ezlapack
-  ! Global module that specify the other modules used in ezlapack
-  ! Allow the user to just import ezlapack and not each module alone for the sake of simplicity
   use module_ezmatmul
   use module_random
 end module ezlapack
