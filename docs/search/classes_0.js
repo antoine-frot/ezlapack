83a0d677abc30d6143cdc183c1cc8f52be02f415
antoine-frot <antoine.frot@orange.fr>
2025-01-10 16:21:12 +0100

    Test for github host page

--- docs/search/classes_0.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['ezmatmul_0',['ezmatmul',['../interfacemodule__ezmatmul_1_1ezmatmul.html',1,'module_ezmatmul']]]
-];
--- docs/search/files_0.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['module_5frandom_2ef90_0',['module_random.f90',['../module__random_8f90.html',1,'']]]
-];
