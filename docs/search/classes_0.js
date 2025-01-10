c1c02f8258a2b18c5aa68cffc257b84dc8483c17
antoine-frot <antoine.frot@orange.fr>
2025-01-10 16:07:00 +0100

    Add HTML documentation

---
+++ docs/search/classes_0.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['ezmatmul_0',['ezmatmul',['../interfacemodule__ezmatmul_1_1ezmatmul.html',1,'module_ezmatmul']]]
+];
---
+++ docs/search/files_1.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['module_5fezmatmul_2ef90_0',['module_ezmatmul.f90',['../module__ezmatmul_8f90.html',1,'']]],
+  ['module_5frandom_2ef90_1',['module_random.f90',['../module__random_8f90.html',1,'']]]
+];
